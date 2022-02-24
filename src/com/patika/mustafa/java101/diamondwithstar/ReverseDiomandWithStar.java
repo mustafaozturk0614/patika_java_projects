9325850c930fde6ea393d4037057aebbc7223515
musty1406 <musty1406@gmail.com>
2022-02-24 10:43:44 +0300

    uml

--- src/com/patika/mustafa/java101/diamondwithstar/ReverseDiomandWithStar.java
+++ src/com/patika/mustafa/java101/diamondwithstar/ReverseDiomandWithStar.java
@@ -9,17 +9,6 @@ public class ReverseDiomandWithStar {
 		System.out.println("lütfen bir sayı giriniz");
 		int number = scanner.nextInt();
 		
-		// for (int i = 1; i <= number; i++) {// for (int i = 1; i <= n; i++)
-		//
-		// for (int j = 1; j <= number - i; j++) {
-		// System.out.print(" ");
-		// }
-		// for (int k = 1; k <= (2 * i - 1); k++) {
-		// System.out.print("*");
-		//
-		// }
-		// System.out.println();
-		// }
 		for (int i = 0; i < number; i++) {
 			String a = "%" + (i + 1) + "" + "s";
 			String data = " ";
